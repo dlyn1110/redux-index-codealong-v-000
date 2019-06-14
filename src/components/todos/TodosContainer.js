7e3c125da6f3982d5f3ddb77a21977b73fdbc527
Dani Lyn <dlyn1110@gmail.com>
2019-06-14 19:34:30 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -2,5 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class TodosContainer extends Component {
-  
-}
+  render() {
+    return(
+      <div></div>
+    );
+  }
+
+};
